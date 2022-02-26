5d90dc57a622fcc5ea9e690d7b30be07206859c3
Poxie <poxen12@hotmail.com>
2022-02-26 17:54:17 +0100

    Added support to api endpoint for images

--- frontend/environment.d.ts
+++ frontend/environment.d.ts
@@ -3,6 +3,7 @@ declare global {
         interface ProcessEnv {
             NODE_ENV: 'development' | 'production';
             NEXT_PUBLIC_API_ENDPOINT: string;
+            NEXT_PUBLIC_API_ORIGIN: string;
         }
     }
 }
--- frontend/utils/constants.ts
+++ frontend/utils/constants.ts
@@ -1,1 +1,2 @@
 export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT as string;
+export const API_ORIGIN = process.env.NEXT_PUBLIC_API_ORIGIN as string;
