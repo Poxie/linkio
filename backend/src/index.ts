2ea595633897f7da8765f59fea8c39cd9890d50a
Poxie <poxen12@hotmail.com>
2022-02-27 18:45:31 +0100

    Created middleware to store context variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,15 +4,21 @@ import resolvers from './resolvers';
 import typeDefs from './typeDefs';
 import mysql2 from 'mysql2';
 import dotenv from 'dotenv';
+import { isAuth } from './isAuth';
+import { ExtendedRequest } from './types';
 dotenv.config();
 
 // Initiating express server
 const app = express();
+app.use(isAuth as any);
 
 // Creataing apollo server
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({ req }: {req: ExtendedRequest}) => ({
+        username: req.username
+    })
 })
 
 // Connecting to MySQL database
---
+++ backend/src/isAuth.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { ExtendedRequest, RequestAuth } from "./types";
+
+const error = (req: ExtendedRequest, next: NextFunction) => {
+    req.isAuth = false;
+    return next();
+}
+export const isAuth = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.get('Authorization');
+    if(!authHeader) return error(req, next);
+
+    const token = authHeader.split(' ')[1];
+    if(!token) return error(req, next);
+
+    // Verifying token
+    let decodedToken: RequestAuth;
+    try {
+        decodedToken = jwt.verify(token, process.env.JSON_WEB_TOKEN_KEY as string) as RequestAuth;
+    } catch(e) {
+        return error(req, next);
+    }
+
+    // Setting context values
+    req.isAuth = true;
+    req.username = decodedToken.username;
+    return next();
+}
