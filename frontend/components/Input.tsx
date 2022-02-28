b6f2a3fe98a85d7c86e43051410f03034af35563
Poxie <poxen12@hotmail.com>
2022-02-28 06:12:37 +0100

    Created input component

---
+++ frontend/components/Input.tsx
@@ -0,0 +1,49 @@
+import React, { useRef, useState } from 'react';
+import styles from '../styles/Input.module.scss';
+
+type Props = {
+    placeholder?: string;
+    value?: string;
+    onChange?: (text: string) => void;
+    onSubmit?: (text: string) => void;
+    clearOnSubmit?: boolean;
+    className?: string;
+}
+export const Input: React.FC<Props> = ({ placeholder, value: _value, onChange, onSubmit, clearOnSubmit, className }) => {
+    const [value, setValue] = useState(_value || '');
+    const ref = useRef<HTMLInputElement>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const val = e.target.value;
+        setValue(val);
+
+        if(onChange) {
+            onChange(val);
+        }
+    }
+    const handleSubmit = (e: React.FormEvent) => {
+        if(!ref.current) return;
+
+        e.preventDefault();
+        if(onSubmit) {
+            onSubmit(value);
+        }
+        
+        if(clearOnSubmit) {
+            setValue('');
+        }
+    }
+
+    className = [styles.form, className].join(' ');
+    return(
+        <form onSubmit={handleSubmit} className={className}>
+            <input 
+                type="text"
+                placeholder={placeholder}
+                onChange={handleChange}
+                value={value}
+                ref={ref}
+            />
+        </form>
+    )
+}
