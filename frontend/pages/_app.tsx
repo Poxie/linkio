69e066a2818930a2c2ea70ff0f5188ad9dc59b9e
Poxie <poxen12@hotmail.com>
2022-02-28 04:22:34 +0100

    Fetching 'me-user' on mount

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,40 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
+import { Provider, useDispatch } from 'react-redux'
 import { store } from '../redux/store'
+import { useEffect } from 'react'
+import { getMe } from '../utils'
+import { setMe } from '../redux/me/userActions'
+import { ReactElement } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <Provider store={store}>
-      <Component {...pageProps} />
+      <AuthLayer>
+        <Component {...pageProps} />
+      </AuthLayer>
     </Provider>
   )
 }
+
+const AuthLayer: React.FC = ({ children }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const accessToken = localStorage.accessToken;
+    if(accessToken) {
+      getMe().then(me => {
+        
+        dispatch(setMe(me));
+
+      }).catch(console.error);
+    }
+  }, []);
+
+  return(
+    <>
+      {children}
+    </>
+  );
+}
+
 export default MyApp
