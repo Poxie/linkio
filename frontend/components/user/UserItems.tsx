d6aacba0bc4f2e747ab52a8d654c23390668a8ff
Poxie <poxen12@hotmail.com>
2022-03-02 12:18:48 +0100

    Only displaying add button and sortable items if 'user is me'

--- frontend/components/user/UserItems.tsx
+++ frontend/components/user/UserItems.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import styles from '../../styles/User.module.scss';
 import { useAppSelector } from '../../redux/store';
-import { selectUserItems } from '../../redux/user/userSelectors';
+import { selectUserIsMe, selectUserItems } from '../../redux/user/userSelectors';
 import { UserItem } from './UserItem';
 import { CreateItemButton } from './CreateItemButton';
 import { SortableItems } from '../SortableItems';
 
 export const UserItems = () => {
     const items = useAppSelector(selectUserItems);
+    const isMe = useAppSelector(selectUserIsMe);
 
     return(
         <>
-        <div className={styles['item-container']}> 
-            <SortableItems 
-                data={items || []}
-                renderComponent={UserItem}
-            />
-        </div>
-        <CreateItemButton />
+            {isMe ? (
+                <>
+                <div className={styles['item-container']}> 
+                    <SortableItems 
+                        data={items || []}
+                        renderComponent={UserItem}
+                    />
+                </div>
+                <CreateItemButton />
+                </>
+            ) : (
+                <div className={styles['item-container']}> 
+                    {items?.map(item => <UserItem {...item} key={item.id} />)}
+                </div>
+            )}
         </>
     )
 }
