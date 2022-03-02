88263be6f2d6fa5da9ec84d8f4dff542a1fbf2e2
Poxie <poxen12@hotmail.com>
2022-03-02 23:23:49 +0100

    Sorting item array on fetch

--- backend/src/resolvers/User.ts
+++ backend/src/resolvers/User.ts
@@ -1,12 +1,15 @@
-import { User } from "../types";
+import { User, UserItem } from "../types";
 import { selectItemsByUserId } from "../utils/databaseActions";
 const IMAGE_ENDPOINT = process.env.IMAGE_ENDPOINT;
 
 export default {
     items: async (parent: User) => {
         const { id } = parent;
-        const items = await selectItemsByUserId(id);
-        return items;
+        const items = await selectItemsByUserId(id) as UserItem[];
+
+        // Ordering item array by order property
+        const sortedItems = items.sort((a,b) => (a.order > b.order) ? 1 : ((b.order > a.order) ? -1 : 0));
+        return sortedItems;
     },
     colorScheme: async (parent: User) => {
         // Getting properties
