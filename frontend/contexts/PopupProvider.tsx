16ec44b9e6bb7f6101eda81278423ed2c432dac8
Poxie <poxen12@hotmail.com>
2022-03-04 22:55:20 +0100

    Added ability to push and go back to previously popup after push

--- frontend/contexts/PopupProvider.tsx
+++ frontend/contexts/PopupProvider.tsx
@@ -17,7 +17,10 @@ type Popup = {
 }
 type PopupContextType = {
     setPopup: (component: Component, ref: Ref ) => void;
+    pushPopup: (component: Component, ref: Ref ) => void;
     closePopups: () => void;
+    goBack: () => void;
+    canGoBack: boolean;
 }
 const PopupContext = React.createContext({} as PopupContextType);
 
@@ -25,6 +28,7 @@ export const usePopup = () => React.useContext(PopupContext);
 
 export const PopupProvider: React.FC = ({ children }) => {
     const [popups, setPopups] = useState<Popup[]>([]);
+    const [activePopup, setActivePopup] = useState(0);
 
     // Get popup position relative to element
     const getPopupPosition = (ref: Element) => {
@@ -54,45 +58,85 @@ export const PopupProvider: React.FC = ({ children }) => {
     }, []);
 
     /**
-     * Sets a popup
+     * Creates a popup object
      * @param component required, the popup component to display
      * @param ref required, the ref of the element the popup should be relative to
-     */
-    const setPopup = (component: Component, ref: Ref) => {
-        closePopups();
+     * @returns a popup object
+    */
+    const createPopup = (component: Component, ref: Ref) => {
         const popup: Popup = {
             id: Math.random(),
             element: ref.current,
             component,
             position: getPopupPosition(ref.current)
         }
+        return popup;
+    }
+    /**
+     * Sets a popup
+     * @param component required, the popup component to display
+     * @param ref required, the ref of the element the popup should be relative to
+     */
+    const setPopup = (component: Component, ref: Ref) => {
+        closePopups();
+        const popup = createPopup(component, ref);
         setPopups(prev => [...prev, popup]);
     }
     /**
+     * Pushs a popup to the array
+     * @param component required, the popup component to display
+     * @param ref required, the ref of the element the popup should be relative to
+    */
+    const pushPopup = (component: Component, ref: Ref) => {
+        const popup = createPopup(component, ref);
+        setPopups(prev => [...prev, popup]);
+        setActivePopup(prev => prev + 1);
+    }
+    /**
+     * Goes to previous popup, if there is one
+    */
+    const goBack = () => {
+        if(popups.length < 2) return;
+
+        setPopups(prev => {
+            const newPopups = [...prev];
+            newPopups.pop();
+            return newPopups;
+        });
+        setActivePopup(prev => prev - 1);
+    }
+    /**
      * Closes the popups
-     */
+    */
     const closePopups = () => {
         setPopups([]);
+        setActivePopup(0);
     }
 
+    const popup = popups[activePopup];
+    const canGoBack = popups.length > 1;
     const value = {
         setPopup,
-        closePopups
+        pushPopup,
+        closePopups,
+        canGoBack,
+        goBack
     }
     return(
         <PopupContext.Provider value={value}>
             {children}
 
             <AnimatePresence>
-                {popups.map(popup => {
-                    const { left, top } = popup.position;
-
-                    return(
-                        <Popup top={top} left={left} key={popup.id}>
-                            {typeof popup.component !== 'function' ? popup.component : <popup.component />}
-                        </Popup>
-                    )
-                })}
+                {popup && (
+                    <Popup 
+                        top={popup.position.top} 
+                        left={popup.position.left}
+                        canGoBack={canGoBack}
+                        key={popup.id}
+                    >
+                        {typeof popup.component !== 'function' ? popup.component : <popup.component />}
+                    </Popup>
+                )}
             </AnimatePresence>
         </PopupContext.Provider>
     )
