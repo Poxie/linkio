c4d95a44d218781b28a0cca52de0d54d0cd48408
Poxie <poxen12@hotmail.com>
2022-03-06 01:48:12 +0100

    Added functionality to upload banner image

--- frontend/components/user/UserBannerCustomize.tsx
+++ frontend/components/user/UserBannerCustomize.tsx
@@ -1,11 +1,13 @@
-import React, { createRef, useCallback, useEffect, useRef } from 'react';
+import React, { createRef, useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useModal } from '../../contexts/ModalProvider';
 import { usePopup } from '../../contexts/PopupProvider';
+import { FileModal } from '../../modals/file-modal/FileModal';
 import { BannerPopup } from '../../popups/banner-popup/BannerPopup';
 import { ColorPopup } from '../../popups/color-popup/ColorPopup';
 import { selectMeId } from '../../redux/me/userSelectors';
 import { useAppSelector } from '../../redux/store';
-import { updateUser as updateUserStore } from '../../redux/user/userActions';
+import { setUser, updateUser as updateUserStore } from '../../redux/user/userActions';
 import { selectUser, selectUserColors } from '../../redux/user/userSelectors';
 import { updateUser } from '../../utils';
 import { User } from '../../utils/types';
@@ -16,8 +18,11 @@ export const UserBannerCustomize = () => {
     const myId = useAppSelector(selectMeId);
     const colors = useAppSelector(selectUserColors);
     const button = createRef<HTMLDivElement>();
+    const input = useRef<HTMLInputElement>(null);
+    const [file, setFile] = useState<File | null>(null);
     const currentButton = useRef<null | HTMLDivElement>(null);
     const { setPopup, pushPopup, closePopups } = usePopup();
+    const { setModal, closeModals } = useModal();
 
     useEffect(() => {
         currentButton.current = button.current;
@@ -45,21 +50,43 @@ export const UserBannerCustomize = () => {
         await updateUser(myId, { bannerColor: color });
     }
 
+    // On image upload
+    const onImageDone = async (file: File) => {
+        if(!myId) return;
+        const user = await updateUser(myId, { banner: file });
+        dispatch(setUser(user));
+        closeModals();
+    }
+    // On image picker change
+    const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(!e.target.files) return;
+
+        const file = e.target.files[0];
+        if(!file) return;
+        
+        setModal(<FileModal file={file} onDone={onImageDone} onCancel={closeModals} />);
+    }
+    // Setting image picker
+    const showImagePicker = () => {
+        closePopups();
+        input.current?.click();
+    } 
     // Setting color picker popup
     const showColorPicker = () => {
         pushPopup(<ColorPopup defaultColor={colors?.background.banner} onChange={setColor} onChangeComplete={updateColor} />, currentButton);
     };
     // Setting banner customization options popup
     const edit = () => {
-        setPopup(<BannerPopup showColorPicker={showColorPicker} />, currentButton);
+        setPopup(<BannerPopup showImagePicker={showImagePicker} showColorPicker={showColorPicker} />, currentButton);
     };
 
     return(
-        <div>
+        <>
             <EditButton 
                 onClick={edit}
                 ref={button}
             />
-        </div>
+            <input type="file" style={{ display: 'none' }} onChange={onFileChange} ref={input} />
+        </>
     )
 }
--- frontend/popups/banner-popup/BannerPopup.tsx
+++ frontend/popups/banner-popup/BannerPopup.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { EditIcon } from '../../icons/EditIcon';
+import { UploadIcon } from '../../icons/UploadIcon';
 import styles from './BannerPopup.module.scss';
 
-export const BannerPopup: React.FC<{showColorPicker: () => void}> = ({ showColorPicker }) => {
+export const BannerPopup: React.FC<{
+    showColorPicker: () => void;
+    showImagePicker: () => void;
+}> = ({ showColorPicker, showImagePicker }) => {
     return(
         <div className={styles.container}>
             <div className={styles.item} onClick={showColorPicker}>
                 <EditIcon />
                 Banner Color
             </div>
-            <div className={styles.item}>
-                <EditIcon />
+            <div className={styles.item} onClick={showImagePicker}>
+                <UploadIcon />
+                Banner Image
             </div>
         </div>
     )
