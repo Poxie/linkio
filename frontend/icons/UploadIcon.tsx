76c8cfa6cdec746d29eeecb0bbf5b3cdaf0c2298
Poxie <poxen12@hotmail.com>
2022-03-06 01:47:48 +0100

    Added upload icon

---
+++ frontend/icons/UploadIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const UploadIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8 10h-5l9-10 9 10h-5v10h-8v-10zm11 9v3h-14v-3h-2v5h18v-5h-2z"/></svg>
+    )
+}
