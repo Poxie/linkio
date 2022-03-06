abdc014951239d24716fe9004f196fdc6a651df9
Poxie <poxen12@hotmail.com>
2022-03-06 01:47:41 +0100

    Added modal for cropping images

---
+++ frontend/modals/file-modal/FileModal.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import styles from './FileModal.module.scss';
+import { ModalHeader } from '../ModalHeader';
+import { Button } from '../../components/Button';
+
+const ASPECT_RATIO = 3 / 1;
+const ROOT_WIDTH = 700;
+export const FileModal: React.FC<{
+    file: File;
+    onCancel?: () => void;
+    onDone?: (file: File) => void;
+}> = ({ file, onCancel, onDone }) => {
+    const [tempImage, setTempImage] = useState<string | null>(null);
+    const [cropper, setCropper] = useState<any>();
+
+    const done = () => {
+        if(!onDone) return;
+
+        cropper.getCroppedCanvas().toBlob((blob: Blob) => {
+            const file = new File([blob], 'file', { type: 'image/png' });
+            onDone(file);
+        })
+    }
+
+    useEffect(() => {
+        const blob = URL.createObjectURL(file);
+        setTempImage(blob);
+    }, [file]);
+
+    return(
+        <div className={styles.container} style={{ position: 'relative' }}>
+            <ModalHeader>
+                Crop Image
+            </ModalHeader>
+            {tempImage && (
+                <Cropper
+                    style={{ height: 400, width: "100%" }}
+                    zoomTo={0.5}
+                    aspectRatio={ASPECT_RATIO}
+                    preview=".img-preview"
+                    src={tempImage}
+                    viewMode={1}
+                    minCropBoxHeight={10}
+                    minCropBoxWidth={10}
+                    background={false}
+                    responsive={true}
+                    autoCropArea={1}
+                    checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                    onInitialized={(instance) => {
+                        setCropper(instance);
+                    }}
+                    guides={true}
+                />
+            )}
+            <div className={styles.footer}>
+                <Button className={styles.button} type={'secondary'} onClick={onCancel}>
+                    Cancel
+                </Button>
+                <Button className={styles.button} onClick={done}>
+                    I'm done
+                </Button>
+            </div>
+        </div>
+    )
+}
