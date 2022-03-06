a0425a7281473b3efce2d1a3636b9b5fe207c0ab
Poxie <poxen12@hotmail.com>
2022-03-06 04:50:32 +0100

    Added extra properties to be able to remove avatar/banner

--- frontend/popups/header-popup/HeaderPopup.tsx
+++ frontend/popups/header-popup/HeaderPopup.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Button } from '../../components/Button';
 import { EditIcon } from '../../icons/EditIcon';
 import { UploadIcon } from '../../icons/UploadIcon';
 import styles from './HeaderPopup.module.scss';
 
 export const HeaderPopup: React.FC<{
+    hasImage?: boolean;
+    onRemove?: () => void;
     showColorPicker?: () => void;
     showImagePicker: () => void;
     type: 'Banner' | 'Avatar';
-}> = ({ showColorPicker, showImagePicker, type }) => {
+}> = ({ showColorPicker, showImagePicker, type, hasImage, onRemove }) => {
     return(
         <div>
             <div className={styles.container}>
@@ -22,6 +25,15 @@ export const HeaderPopup: React.FC<{
                     {type} Image
                 </div>
             </div>
+            {hasImage && (
+                <Button 
+                    className={styles['remove-button']}
+                    type={'secondary'}
+                    onClick={onRemove}
+                >
+                    Remove {type}
+                </Button>
+            )}
         </div>
     )
 }
