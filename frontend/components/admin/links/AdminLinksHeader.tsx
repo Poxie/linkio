1fe40991f0706d4ab40188d36421e30387a64be3
Poxie <poxen12@hotmail.com>
2022-03-28 17:22:43 +0200

    Added basic admin links header

--- frontend/components/admin/links/AdminLinksHeader.tsx
+++ frontend/components/admin/links/AdminLinksHeader.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Button } from '../../Button';
+import styles from '../../../styles/Admin.module.scss';
 
 export const AdminLinksHeader = () => {
     return(
-        <div>
-            heloy
+        <div className={styles['links-header']}>
+            <span className={styles['links-header-text']}>
+                My Links
+            </span>
+            <div className={styles['links-header-main']}>
+                <Button>
+                    Add New Link
+                </Button>
+                <Button>
+                    Add With Integration
+                </Button>
+            </div>
         </div>
     )
 }
