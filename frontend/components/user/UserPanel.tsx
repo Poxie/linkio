e43a5213757bb64c950932baff7ed76919345802
Poxie <poxen12@hotmail.com>
2022-03-28 14:11:07 +0200

    Added path for admin panel button

--- frontend/components/user/UserPanel.tsx
+++ frontend/components/user/UserPanel.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { DrawnArrowIcon } from '../../icons/DrawnArrowIcon';
 import styles from '../../styles/User.module.scss';
 import { Button } from '../Button';
@@ -6,9 +7,11 @@ import { Button } from '../Button';
 export const UserPanel = () => {
     return(
         <div className={styles['user-panel']}>
-            <Button>
-                Admin Panel
-            </Button>
+            <Link href={'/admin'}>
+                <Button>
+                    Admin Panel
+                </Button>
+            </Link>
             <DrawnArrowIcon />
             <span>
                 Open the admin panel for more customization and analytics.
