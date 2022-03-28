87f5ad7bc8bdf28745a210b5ba188cf72bbadd81
Poxie <poxen12@hotmail.com>
2022-03-28 17:40:54 +0200

    Added function to create new link item

--- frontend/components/admin/links/AdminLinksHeader.tsx
+++ frontend/components/admin/links/AdminLinksHeader.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Button } from '../../Button';
 import styles from '../../../styles/Admin.module.scss';
+import { useDispatch } from 'react-redux';
+import { setMeItems } from '../../../redux/me/meActions';
+import { useAppSelector } from '../../../redux/store';
+import { selectMeId, selectMeItems } from '../../../redux/me/meSelectors';
+import { createUserItem } from '../../../utils';
 
 export const AdminLinksHeader = () => {
+    const dispatch = useDispatch();
+    const myItems = useAppSelector(selectMeItems);
+    const myId = useAppSelector(selectMeId);
+
+    const addNewLink = async () => {
+        if(!myItems || !myId) return;
+
+        // Creating an empty item for the user to update
+        const item = await createUserItem({ content: '', url: '', userId: myId });
+
+        // Pushing new item to redux
+        dispatch(setMeItems([...myItems, ...[item]]));
+    }
+    const addWithIntegration = () => {
+
+    }
+
     return(
         <div className={styles['links-header']}>
             <span className={styles['links-header-text']}>
                 My Links
             </span>
             <div className={styles['links-header-main']}>
-                <Button>
+                <Button onClick={addNewLink}>
                     Add New Link
                 </Button>
-                <Button>
+                <Button onClick={addWithIntegration}>
                     Add With Integration
                 </Button>
             </div>
