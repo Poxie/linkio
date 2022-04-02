8f787c701b785258025a8eb2302ba63e4b0f78d4
Poxie <poxen12@hotmail.com>
2022-04-03 00:01:12 +0200

    Added additional me selectors

--- frontend/redux/me/meSelectors.ts
+++ frontend/redux/me/meSelectors.ts
@@ -1,6 +1,18 @@
+import { createSelector } from "reselect";
 import { RootState } from "../store";
 
 export const selectMe = (state: RootState) => state.me.user;
 export const selectMeId = (state: RootState) => state.me.user?.id;
 export const selectMeItems = (state: RootState) => state.me.user?.items;
+export const selectMeColors = (state: RootState) => state.me.user?.colorScheme;
 export const selectMeLoading = (state: RootState) => state.me.loading;
+
+export const selectMeDisplay = createSelector(
+    [selectMe],
+    (user) => ({
+        username: user?.username,
+        name: user?.name,
+        bannerURL: user?.bannerURL,
+        avatarURL: user?.avatarURL,
+    })
+)
