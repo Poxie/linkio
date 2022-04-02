44e58c73b236d48c7fd67cf25c863096d3a363da
Poxie <poxen12@hotmail.com>
2022-04-02 22:56:48 +0200

    Added ability to wait before showing tooltip

--- frontend/components/tooltip/HasTooltip.tsx
+++ frontend/components/tooltip/HasTooltip.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useRef } from 'react';
+import React, { ReactElement, useCallback, useRef } from 'react';
 import { useTooltip } from '../../contexts/TooltipProvider';
 
 export const HasTooltip: React.FC<{
@@ -8,13 +8,29 @@ export const HasTooltip: React.FC<{
     delay?: number;
 }> = ({ children, tooltip, closeOnClick, delay=0 }) => {
     const { setTooltip, closeTooltip } = useTooltip();
+    const hoveringNow = useRef(false);
     const ref = useRef<HTMLDivElement>(null);
+    const timeout = useRef<NodeJS.Timeout | null>(null);
 
     const open = () => setTooltip({ tooltip, elementRef: ref });
     const close = closeTooltip;
 
+    const handleMouseEnter = useCallback(() => {
+        hoveringNow.current = true;
+        if(!delay) return open();
+        
+        timeout.current = setTimeout(() => {
+            if(hoveringNow.current) open();
+        }, delay);
+    }, [open, delay]);
+    const handleMouseLeave = useCallback(() => {
+        hoveringNow.current = false;
+        timeout.current && clearTimeout(timeout.current);
+        close();
+    }, [close]);
+
     return(
-        <div onMouseEnter={open} onMouseLeave={close} ref={ref}>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ref={ref}>
             {children}
         </div>
     )
