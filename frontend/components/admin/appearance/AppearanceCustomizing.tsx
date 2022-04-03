0d9ae0a5d3b450c723b0c4c280b87480f20625f1
Poxie <poxen12@hotmail.com>
2022-04-03 05:00:51 +0200

    Updating database on color change

--- frontend/components/admin/appearance/AppearanceCustomizing.tsx
+++ frontend/components/admin/appearance/AppearanceCustomizing.tsx
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from '../../../styles/Admin.module.scss';
 import { useDispatch } from 'react-redux';
 import { setMeColor } from '../../../redux/me/meActions';
-import { selectMeColors } from '../../../redux/me/meSelectors';
+import { selectMeColors, selectMeId } from '../../../redux/me/meSelectors';
 import { useAppSelector } from '../../../redux/store';
 import { CutsomizeColorInput } from './CustomizeColorInput';
+import { User } from '../../../utils/types';
+import { updateUser } from '../../../utils';
 
 export const AppearanceCustomizing = () => {
     const dispatch = useDispatch();
+    const id = useAppSelector(selectMeId);
     const colors = useAppSelector(selectMeColors);
+
+    const updateColor = useCallback(async (type: keyof User['colorScheme']['background'], color: string) => {
+        if(!id) return;
+
+        dispatch(setMeColor('background', type, color));
+        await updateUser(id, { [`${type}Color`]: color });
+    }, [dispatch, id]);
 
     return(
         <div className={styles['customize-container']}>
             <CutsomizeColorInput 
                 value={colors?.background?.banner || 'var(--background-secondary)'}
                 header={'Banner Color'}
-                onChange={value => dispatch(setMeColor('background', 'banner', value))}
+                onChange={value => updateColor('banner', value)}
             />
             <CutsomizeColorInput 
                 value={colors?.background?.header || 'var(--background-primary)'}
                 header={'Header Color'}
-                onChange={value => dispatch(setMeColor('background', 'header', value))}
+                onChange={value => updateColor('header', value)}
             />
             <CutsomizeColorInput 
                 value={colors?.background?.item || 'var(--background-primary)'}
                 header={'Item Color'}
-                onChange={value => dispatch(setMeColor('background', 'item', value))}
+                onChange={value => updateColor('item', value)}
             />
             <CutsomizeColorInput 
                 value={colors?.background?.primary || 'var(--background-secondary)'}
                 header={'Background Color'}
-                onChange={value => dispatch(setMeColor('background', 'primary', value))}
+                onChange={value => updateColor('primary', value)}
             />
         </div>
     )
