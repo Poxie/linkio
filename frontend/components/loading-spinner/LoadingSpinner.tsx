a741cbe268617d066dbd5802a3ea1552750aeb95
Poxie <poxen12@hotmail.com>
2022-04-06 13:10:01 +0200

    Added className property

--- frontend/components/loading-spinner/LoadingSpinner.tsx
+++ frontend/components/loading-spinner/LoadingSpinner.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './LoadingSpinner.module.scss';
 
-export const LoadingSpinner = () => {
+export const LoadingSpinner: React.FC<{
+    className?: string
+}> = ({ className='' }) => {
+    className = [styles['svg'], className].join(' ');
     return(
-        <svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg" className={styles['svg']}>
+        <svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg" className={className}>
             <circle strokeLinecap={'round'} cx="60" cy="60" r="46" className={styles['circle']} />
         </svg>
     )
