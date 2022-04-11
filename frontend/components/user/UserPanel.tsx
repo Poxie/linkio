fb5b2790425df0e0fe81765dd3af7876cea5b984
Poxie <poxen12@hotmail.com>
2022-04-11 07:43:17 +0200

    Fixed error with nested links

--- frontend/components/user/UserPanel.tsx
+++ frontend/components/user/UserPanel.tsx
@@ -7,11 +7,11 @@ import { Button } from '../Button';
 export const UserPanel = () => {
     return(
         <div className={styles['user-panel']}>
-            <Link href={'/admin'}>
-                <Button>
+            <Button style={{ height: '45px', width: '128px', padding: 0 }}>
+                <Link href={'/admin'}>
                     Admin Panel
-                </Button>
-            </Link>
+                </Link>
+            </Button>
             <DrawnArrowIcon />
             <span>
                 Open the admin panel for more customization and analytics.
