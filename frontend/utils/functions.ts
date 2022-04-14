a1a94024063af8fb75ff5555cad1be01f654549f
Poxie <poxen12@hotmail.com>
2022-04-14 22:59:24 +0200

    Added utility function

--- frontend/utils/functions.ts
+++ frontend/utils/functions.ts
@@ -3,3 +3,6 @@ import { IMAGE_ENDPOINT } from "./constants"
 export const getIconURL = (icon: string) => {
     return `${IMAGE_ENDPOINT}/icons/${icon}.png`
 }
+export const capitalizeFirstLetter = (str: string) => {
+    return str.slice(0,1).toUpperCase() + str.slice(1);
+}
