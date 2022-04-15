a856a8bd37ef0848f7eb96c1d8926f5caef9661f
Poxie <poxen12@hotmail.com>
2022-04-15 02:00:25 +0200

    Added empty items notice for non-logged in users

--- frontend/components/user/UserItems.tsx
+++ frontend/components/user/UserItems.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../../styles/User.module.scss';
 import { useAppSelector } from '../../redux/store';
-import { selectUserIsMe, selectUserItems } from '../../redux/user/userSelectors';
+import { selectUserDisplay, selectUserIsMe, selectUserItems } from '../../redux/user/userSelectors';
 import { UserItem } from './UserItem';
 import { CreateItemButton } from './CreateItemButton';
 import { Item } from '../../utils/types';
@@ -14,7 +14,8 @@ import { setMeItems } from '../../redux/me/meActions';
 
 export const UserItems = () => {
     const dispatch = useDispatch();
-    const items = (useAppSelector(selectUserItems) || []).sort((a,b) => a.order - b.order);
+    const { name, username } = useAppSelector(selectUserDisplay);
+    const items = useAppSelector(selectUserItems) || [];
     const isMe = useAppSelector(selectUserIsMe);
     const myId = useAppSelector(selectMeId);
 
@@ -45,6 +46,12 @@ export const UserItems = () => {
             ) : (
                 <div className={styles['item-container']}> 
                     {items?.map(item => <UserItem {...item} key={item.id} />)}
+
+                    {items.length === 0 && (
+                        <div className={styles['empty-items']}>
+                            {name || `@${username}`} doesn't have any items yet. :(
+                        </div>
+                    )}
                 </div>
             )}
         </>
